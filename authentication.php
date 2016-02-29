d84ecbf4ea4863c8cec1733ae8be7727147c0107
khalid <khalid@khalids-MacBook-Pro.local>
2016-02-29 19:55:26 +0400

    updates

--- authentication.php
+++ authentication.php
@@ -2,40 +2,38 @@
 
 include 'database_connection.php';
 
-$request_type = $_GET['request_type'];
+$request_type = $_POST['request_type'];
 
 echo $request_type;
 
 if($request_type == "create_user"){
-            $sql = "INSERT INTO USER_ACCOUNTS (uname, fname, lname,email,pass,prof_id,acctype_id,acc_statid)
-        VALUES ('John', 'Doe',,'Doe', 'john@example.com','somepass',1,2,3)";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
+       create_user_request();    
 }
 
 
-function fetch_create_user_request(){
+function create_user_request(){
     $uname = $_POST['uname'];
 $fname= $_POST['fname'];
 $lname= $_POST['lname'];
 $email= $_POST['email'];
 $pass= $_POST['pass'];
-$prof_id= $_POST['prof_id'];
-$acctype_id= $_POST['acctype_id'];
-$acc_statid= $_POST['acc_statid'];
 
 
+echo $email . '\n';
+echo $pass . '\n';
 create_user($uname,$fname,$lname,$email,$pass,$prof_id,$acctype_id,$acc_statid);
 
 }
 
 function create_user($uname,$fname,$lname,$email,$pass,$prof_id,$acctype_id,$acc_statid){
     
-    
+     $sql = "INSERT INTO USER_ACCOUNTS (uname, fname, lname,email,pass,prof_id,acctype_id,acc_statid) VALUES ('" . $uname . "', '" . $fname . "','" . $lname . "', '" . $email . "','" . $pass . "',1,2,3)";
+
+        if (mysql_query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
 
     
 }
