027ac6879f28ef0b2d933d6aff246c2d42a112d3
khalid <khalid@khalids-MacBook-Pro.local>
2016-03-14 01:28:17 +0400

    tokenage

--- authentication.php
+++ authentication.php
@@ -6,6 +6,8 @@ $request_type = $_POST['request_type'];
 
 echo $request_type;
 
+$prof_id = 0;
+
 if($request_type == "create_user"){
        create_user_request();    
 }else if($request_type == "authenticate_user"){
@@ -37,17 +39,11 @@ create_user($uname,$fname,$lname,$email,$pass,$phone,$prof_id,$acctype_id,$acc_s
 function create_profile($fname,$lname){
     $sql = "INSERT INTO PROFILES (name) VALUES ('" . $fname ." ". $lname . "')";
     if (mysql_query($sql) === TRUE) {
-            echo "New profile created successfully";
+            echo "New profile created successfully\n";
         } else {   
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
-    $sql = "SELECT SCOPE_IDENTITY()";
-     if (mysql_query($sql) === TRUE) {
-            echo "retrieved last ID  successfully";
-        } else {   
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-        
+    
             
         
         
@@ -55,7 +51,14 @@ function create_profile($fname,$lname){
 
 function create_user($uname,$fname,$lname,$email,$pass,$phone , $prof_id,$acctype_id,$acc_statid){
     
-     $sql = "INSERT INTO USER_ACCOUNTS (uname, fname, lname , email , phone_number , pass , prof_id , acctype_id , acc_statid ) VALUES ('" . $uname . "', '" . $fname . "','" . $lname . "', '" . $email . "','" . $phone . "','" . $pass . "',1,2,3)";
+            $sql = "SELECT LAST_INSERT_ID() as id";               //future race condition
+                $result = mysql_query($sql);
+                $result = mysql_fetch_array($result);
+            $prof_id =  $result['id'];      
+                
+                
+                
+     $sql = "INSERT INTO USER_ACCOUNTS (uname, fname, lname , email , phone_number , pass , prof_id , acctype_id , acc_statid ) VALUES ('" . $uname . "', '" . $fname . "','" . $lname . "', '" . $email . "','" . $phone . "','" . $pass . "','". $prof_id ."',2,3)";
 
         if (mysql_query($sql) === TRUE) {
             echo "New record created successfully";
@@ -64,6 +67,16 @@ function create_user($uname,$fname,$lname,$email,$pass,$phone , $prof_id,$acctyp
         }
 
     
+}
+
+function generateRandomString($length = 50) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
 }
 
 
@@ -75,12 +88,37 @@ function authenticate_user($uname,$pass){
     
     if (mysql_num_rows($result)==0) {  
         
-        
+        echo 'no user/password combination found';
         
      }else{
 
             while($row = mysql_fetch_array($result)) {
-                print_r($row);
+                 
+                 $rand_string = generateRandomString();
+                 
+               $sql = "INSERT INTO TOKENS (token,last_request) VALUES ('". $rand_string ."',now())";
+             
+                        if (mysql_query($sql) === TRUE) {
+                            echo "Token created successfully";
+                            
+                             $sql = "SELECT LAST_INSERT_ID() as id";               //future race condition
+                                $result = mysql_query($sql);
+                                $result = mysql_fetch_array($result);
+                            $tokid =  $result['id'];     
+                            
+                            
+                            $update_query = "UPDATE USER_ACCOUNTS SET tokid='". $tokid ."' WHERE uname='".$uname . "'";
+                            if (mysql_query($update_query) === TRUE) {
+                                echo "tokid updated successfully";
+
+                            }else {   
+                                echo "Error: " . $update_query . "<br>" . $conn->error;
+                            }
+                        } else {   
+                            echo "Error: " . $sql . "<br>" . $conn->error;
+                        }
+               
+               
             }
      }
    
