ab67b3bf19e41a1f9281436b6f568e6cad9f0df8
khalid <khalid@khalids-MacBook-Pro.local>
2016-04-05 13:17:49 +0400

    k

--- editProfile.php
+++ editProfile.php
@@ -1,16 +1,25 @@
 <?php
 
+include_once 'database_connection.php';
 
-updateProfileInfo($_POST['prof_id'], $_POST['location'], $_POST['name'], $_POST['description']));
+updateProfileInfo($_POST['accid'], $_POST['name'], $_POST['description']);
 
 
-function updateProfileInfo($prof_id, $loc, $name, $desc){
+function updateProfileInfo($accid, $name, $desc){
+    $sql = "SELECT prof_id FROM USER_ACCOUNTS where accid=". $accid;
+    $result = mysql_query($sql);
+    $result = mysql_fetch_array($result);
+    
+    $prof_id = $result['prof_id'];
+    
     $sql = "UPDATE PROFILES
-            SET location='" . $loc . "', name='" . $name . "', description='" . $desc . "'
+            SET  name='" . $name . "', description='" . $desc . "'
             WHERE prof_id='" . $prof_id . "'";
     if(!mysql_query($sql)){
         echo "Error: could not update profile";
-    }   
+    }else{
+        echo 'update successful';
+    }
 }
 
 ?>
--- getProfile.php
+++ getProfile.php
@@ -1,5 +1,6 @@
 <?php
 
+include_once 'database_connection.php';
 
 echo json_encode(getProfile($_POST['accid']));
 
@@ -10,13 +11,13 @@ function getProfile($accid){
     
     $sql = "SELECT prof_id FROM USER_ACCOUNTS where accid=". $accid;
     $result = mysql_query($sql);
-    $result = mysql_fetch_array($sql);
+    $result = mysql_fetch_array($result);
     
-    
+    $prof_id = $result['prof_id'];
     
     $sql = "SELECT location, name, description, media_id FROM PROFILES WHERE prof_id='" . $prof_id . "'";
     $result = mysql_query($sql);
-    $result = mysql_fetch_assoc($result);
+    $result = mysql_fetch_array($result);
     
     
     $profile = array([
