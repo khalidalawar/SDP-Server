dcb0a53d22b66c1964e83b1a2ea55aadd3a01cc8
khalid <khalid@khalids-MacBook-Pro.local>
2016-04-12 10:45:26 +0400

    a

--- createJob.php
+++ createJob.php
@@ -1,12 +1,16 @@
 <?php
 
-insertJob($_POST['cust_accid'], $_POST['desc'], $_POST['subcat_id'], $_POST['lifetime'], $_POST['job_statid']);
+
+
+insertJob($_POST['accid'], $_POST['desc'], $_POST['subcat_id'], $_POST['lifetime'], $_POST['job_statid']);
 
 function insertJob($cust_accid, $desc, $subcat_id, $lifetime, $job_statid){
     $insert = "INSERT INTO JOBS (ts, cust_accid, description, subcat_id, lifetime, job_statid)
                 VALUES (now(), ". $cust_accid .", ". $desc .", ". $subcat_id .", ". $lifetime .", ". $job_statid .")";
     if(!mysql_query($insert)){
         echo "Error: could not insert job!";
+    }else{
+        echo 'successfuly inserted job';
     }
 }
 
