ad5e17f3e29d472022cfb67af7b14b22e92be107
khalid <khalid@khalids-MacBook-Pro.local>
2016-04-13 14:16:25 +0400

    title

--- createJob.php
+++ createJob.php
@@ -1,12 +1,12 @@
 <?php
 
+include_once 'database_connection.php';
 
+insertJob($_POST['accid'],$_POST['title'], $_POST['desc'], $_POST['subcat_id'], $_POST['lifetime']);// it is initally 1, $_POST['job_statid']);
 
-insertJob($_POST['accid'], $_POST['desc'], $_POST['subcat_id'], $_POST['lifetime'], $_POST['job_statid']);
-
-function insertJob($cust_accid, $desc, $subcat_id, $lifetime, $job_statid){
-    $insert = "INSERT INTO JOBS (ts, cust_accid, description, subcat_id, lifetime, job_statid)
-                VALUES (now(), ". $cust_accid .", ". $desc .", ". $subcat_id .", ". $lifetime .", ". $job_statid .")";
+function insertJob($cust_accid,$title, $desc, $subcat_id, $lifetime, $job_statid){
+    $insert = "INSERT INTO JOBS (ts, cust_accid,title, description, subcat_id, lifetime, job_statid)
+                VALUES (now(), ". $cust_accid .",".$title.", ". $desc .", ". $subcat_id .", ". $lifetime .", 1)";
     if(!mysql_query($insert)){
         echo "Error: could not insert job!";
     }else{
